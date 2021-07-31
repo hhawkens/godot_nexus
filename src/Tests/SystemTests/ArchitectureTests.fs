0946539e77047a1b99920dcc2d024f57ae7f50ae
john <mail@mail.com>
2021-08-01 00:16:45 +0200

    Adjusted arch tests to new shell layer

--- src/Tests/SystemTests/ArchitectureTests.fs
+++ src/Tests/SystemTests/ArchitectureTests.fs
@@ -37,27 +37,37 @@ let public ``All Layers Only Reference Lower Level Layers`` () =
 
     let utilsLayer = Types().That().ResideInAssembly(@"App.Utilities*", true).As("Utils Layer")
     let coreLayer = Types().That().ResideInAssembly(@"App.Core*", true).As("Core Layer")
+    let shellLayer = Types().That().ResideInAssembly(@"App.Shell*", true).As("Shell Layer")
     let frontendLayer = Types().That().ResideInAssembly(@"App.Presentation.Frontend").As("Frontend Layer")
     let guiLayer = Types().That().ResideInAssembly(@"App.Presentation.Gui").As("GUI Layer")
     let mainLayer = Types().That().ResideInAssembly(@"App.Main*", true).As("Main Layer")
 
     utilsLayer |> shouldExist
     coreLayer |> shouldExist
+    shellLayer |> shouldExist
     frontendLayer |> shouldExist
     guiLayer |> shouldExist
     mainLayer |> shouldExist
 
     utilsLayer |> shouldNotDependOn coreLayer
+    utilsLayer |> shouldNotDependOn shellLayer
     utilsLayer |> shouldNotDependOn frontendLayer
     utilsLayer |> shouldNotDependOn guiLayer
     utilsLayer |> shouldNotDependOn mainLayer
 
+    coreLayer |> shouldNotDependOn shellLayer
     coreLayer |> shouldNotDependOn frontendLayer
     coreLayer |> shouldNotDependOn guiLayer
     coreLayer |> shouldNotDependOn mainLayer
+
+    shellLayer |> shouldNotDependOn frontendLayer
+    shellLayer |> shouldNotDependOn guiLayer
+    shellLayer |> shouldNotDependOn mainLayer
 
     frontendLayer |> shouldNotDependOn mainLayer
     frontendLayer |> shouldNotDependOn guiLayer
 
     guiLayer |> shouldNotDependOn mainLayer
-    guiLayer |> shouldNotDependOn coreLayer // This is because we are using closed layers
+    // The following are necessary because we are using closed layers
+    guiLayer |> shouldNotDependOn coreLayer
+    guiLayer |> shouldNotDependOn shellLayer
